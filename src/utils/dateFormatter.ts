fe847cf7d60887681f74cc172e1f9a90e6cb3fbd
GianLaolao <gian_laolao@dlsu.edu.ph>
2026-01-18 11:45:28 +0800

    added utility files
    dateFormatter
    - formatting timestamp to a date string
    
    localizeTime
    - converting time to local timezone
    
    removeDupes
    - removed duplicate data entries

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,7 +1,12 @@
 
 import { parseISO, format } from "date-fns";
 
-export function formatDate(isoDate: string): string {
+export function formatDate(isoDate: string | null): string {
+
+  if (!isoDate) {
+    return "---";
+  }
+
   const date = parseISO(isoDate);
   return format(date, "MMM. d, yyyy");
 }
