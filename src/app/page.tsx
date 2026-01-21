7da6959686553532b277bc4f83c5dfcddfb666d3
GianLaolao <gian_laolao@dlsu.edu.ph>
2026-01-21 18:04:55 +0800

    removes items in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import { FollowButton } from "@/ui";
-import { InfoCard } from "@/components";
-import { getAnimeInfo } from "@/lib/getAnimeInfo";
-
 
 export default async function Home() {
 
-  const sampleAnime = await getAnimeInfo(52991); 
 
   return (
     <>
-      <main className="flex h-screen flex-col items-center justify-between">
-        <h1 className="text-4xl font-bold mb-8">Welcome to</h1>
-        <InfoCard anime={sampleAnime} />
-      </main>
     </>    
   );
 }
