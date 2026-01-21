481c505c50ada5350d6bdf3df373efe99812c43d
GianLaolao <gian_laolao@dlsu.edu.ph>
2026-01-21 18:03:16 +0800

    added logo to header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -4,6 +4,7 @@
 import { UserRound } from "lucide-react";
 import { ThemeToggle, SearchBar } from "@/ui";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Header() {
 
@@ -15,8 +16,11 @@ export default function Header() {
 
     return (
         <div className="navbar bg-sky-950 shadow-sm sticky top-0 z-50 px-20 w-full shadow-base-content/20 items-center">
-            <div className="flex-1 hover:cursor-pointer" onClick={redirectHome}>
-                <a className="text-3xl text-white font-(--font-zen-kaku) tracking-[0.06em]" >Turaka</a>
+            <div className="flex flex-1 hover:cursor-pointer flex-row gap-5" onClick={redirectHome}>
+                <div className="rounded-full w-max">
+                    <Image src="/images/logo.png" className="rounded-full" alt="KamiLog Logo" width={45} height={45} />
+                </div>
+                <a className="text-3xl text-white font-(--font-zen-kaku) font-bont tracking-[0.06em]" >KamiLog</a>
             </div>
             <div className="flex w-max gap-8 justify-items-center">
                 <SearchBar size={"sm"} />
