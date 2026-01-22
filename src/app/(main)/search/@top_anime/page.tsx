33cce4fa0b0157c43fdaf425924845ed337c4dca
GianLaolao <gian_laolao@dlsu.edu.ph>
2026-01-22 11:01:23 +0800

    optimized fetching

--- src/app/(main)/search/@top_anime/page.tsx
+++ src/app/(main)/search/@top_anime/page.tsx
@@ -1,14 +1,36 @@
 
+'use client';
+
 import { getTopAnime } from "@/lib/getAnimeInfo";
 import { Anime } from "@/types/animeTypes";
 import { Rankings } from "@/components";
+import { useEffect, useState } from "react";
+import { delay } from "@/utils/delay";
 
+export default function TopAnime() {
 
-export default async function TopAnime() {
+    // const topAnime: Anime[] = await getTopAnime("tv").then(data => data.data);
+    // const topUpcoming: Anime[] = await getTopAnime("tv", "upcoming").then(data => data.data);
 
-    const topAnime: Anime[] = await getTopAnime("tv").then(data => data.data);
-    const topUpcoming: Anime[] = await getTopAnime("tv", "upcoming").then(data => data.data);
+    const [topAnime, setTopAnime] = useState<Anime[]>([]);
+    const [topUpcoming, setTopUpcoming] = useState<Anime[]>([]);
+    
+    useEffect(() => {
 
+        const fetchAnimes = async () => {
+            const [topAnimeData, topUpcomingData] = await Promise.all([
+                getTopAnime("tv").then(data => data.data),
+                getTopAnime("tv", "upcoming").then(data => data.data)
+            ]);
+            await delay();
+            setTopAnime(topAnimeData);
+            setTopUpcoming(topUpcomingData);
+        };
+        fetchAnimes();
+        
+    }, []);
+
+    
     return (
         <div className="h-full w-full p-2 px-3 pb-5 flex flex-col gap-2 items-center justify-items-center border-l-2 border-base-300">
             <Rankings title="Top Anime" anime={topAnime} />
